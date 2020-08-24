1df37f6eb2a0284a8865959defa979e83843da82
budhi arta <budhiarta@budhis-MacBook-Pro.local>
2020-08-24 14:49:48 +0800

    regex3

---
+++ index.js
@@ -0,0 +1,10 @@
+let search = /^Bagaimana memulai usaha bisnis$|^mulai bisnis$/i;
+console.log(search.test('mulai bisnis'))
+
+
+let search2 = /^Bootcamp impact byte$|^impact byte$/i;
+console.log(search.test('impact byte'))
+
+
+let search3 = /^Status covid hari ini$|^status covid$/i;
+console.log(search.test('status covid'))
